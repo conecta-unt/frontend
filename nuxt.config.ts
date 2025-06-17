4c5fd3b3308b9c4b8d28481cd0140375e38333db
angelchavezinformatica <achavezg@unitru.edu.pe>
2025-06-16 23:57:22 -0500

    feat(landing-page): hero section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@nuxt/icon", "@nuxt/image"],
   css: ["~/assets/css/main.css"],
   dir: { pages: "routes" },
---
+++ routes/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+import Page from "~/pages/index.vue";
+</script>
+
+<template><Page /></template>
