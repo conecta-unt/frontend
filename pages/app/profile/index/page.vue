1420e1c05a85663b9adba74b197b76b4f742dd9a
angelchavez19 <infoangelchavez@gmail.com>
2025-07-08 00:51:50 -0500

    feat(user): get profile by user query

--- pages/app/profile/index/page.vue
+++ pages/app/profile/index/page.vue
@@ -3,7 +3,9 @@ import { z } from "zod/v4";
 import { areaSchema, bioSchema, majorSchema } from "~/schemas";
 import type { UserProfileI } from "~/types/user";
 
+const route = useRoute();
 const user = useState<UserProfileI>("user");
+const profile = ref<UserProfileI | null>();
 const { $axios } = useNuxtApp();
 
 const roles: any = {
@@ -48,66 +50,104 @@ const edit = async () => {
   }
 };
 
-watch(user, () => {
+const getUserProfile = async () => {
+  if (!user.value) return;
+
+  const q = route.query.user;
+  if (!q || user.value.username === q) profile.value = user.value;
+  else {
+    try {
+      const result = await $axios.get<UserProfileI>(
+        `/user/info/profile?q=${q}`
+      );
+      profile.value = result.data;
+
+      if (!profile.value.profileImage) {
+        const name =
+          `${profile.value.firstname} ${profile.value.lastname}`.replace(
+            " ",
+            "+"
+          );
+        profile.value.profileImage = `https://ui-avatars.com/api/?name=${name}&size=60&background=random&bold=true`;
+      }
+    } catch {
+      profile.value = null;
+    }
+  }
+
+  if (!profile.value) {
+    useSeoMeta({
+      title: `${q} not found | Conecta UNT`,
+    });
+    return;
+  }
+
   useSeoMeta({
-    title: `${user.value.username} (${user.value.firstname} ${user.value.lastname}) | Conecta UNT`,
+    title: `${profile.value.username} (${profile.value.firstname} ${profile.value.lastname}) | Conecta UNT`,
   });
 
-  data.major = user.value.major || undefined;
-  data.area = user.value.area || undefined;
-  data.bio = user.value.bio || undefined;
+  data.major = profile.value.major || undefined;
+  data.area = profile.value.area || undefined;
+  data.bio = profile.value.bio || undefined;
+};
+
+watch(user, async () => {
+  await getUserProfile();
 });
 </script>
 
 <template>
   <NuxtLayout name="app">
-    <div>
+    <div v-if="user && profile">
       <section class="flex flex-col gap-6">
         <div class="flex items-center gap-4">
           <div
             class="flex items-center justify-center rounded-full outline-1 outline-black overflow-hidden"
           >
             <img
-              :src="user.profileImage || undefined"
+              :src="profile.profileImage || undefined"
               width="60"
               alt="Foto de perfil"
             />
           </div>
           <div>
-            <h1 class="text-2xl">{{ user.firstname }} {{ user.lastname }}</h1>
-            <p>@{{ user.username }}</p>
+            <h1 class="text-2xl">
+              {{ profile.firstname }} {{ profile.lastname }}
+            </h1>
+            <p>@{{ profile.username }}</p>
           </div>
         </div>
 
         <div class="flex flex-col gap-2">
           <p>
             Cuenta de
-            <span class="text-(--c-text-2)">{{ roles[user.role] }}</span>
+            <span class="text-(--c-text-2)">{{ roles[profile.role] }}</span>
           </p>
           <template v-if="!isEditing">
-            <p v-if="user.major">
-              <span class="font-extrabold">Carrera:</span> {{ user.major }}
+            <p v-if="profile.major">
+              <span class="font-extrabold">Carrera:</span> {{ profile.major }}
             </p>
 
-            <p v-if="user.area">
+            <p v-if="profile.area">
               <span
-                v-if="user.role === 'bussines_client'"
+                v-if="profile.role === 'bussines_client'"
                 class="font-extrabold"
-                >Rubro:</span
               >
+                Rubro:
+              </span>
               <span v-else class="font-extrabold">Especialidad:</span>
-              {{ user.area }}
+              {{ profile.area }}
             </p>
 
-            <p v-if="user.bio">
-              {{ user.bio }}
+            <p v-if="profile.bio">
+              {{ profile.bio }}
             </p>
           </template>
         </div>
 
         <div v-if="isEditing" class="flex flex-col gap-4">
           <InputText
-            v-if="user.role !== 'bussines_client'"
+            v-if="profile.role !== 'bussines_client'"
             v-model="data.major"
             label="Carrera"
             class="max-w-md"
@@ -115,7 +155,9 @@ watch(user, () => {
           />
           <InputText
             v-model="data.area"
-            :label="user.role === 'bussines_client' ? 'Rubro' : 'Especialidad'"
+            :label="
+              profile.role === 'bussines_client' ? 'Rubro' : 'Especialidad'
+            "
             class="max-w-md"
             :schema="areaSchema"
           />
@@ -126,7 +168,7 @@ watch(user, () => {
             :schema="bioSchema"
           />
         </div>
-        <div>
+        <div v-if="user.username === profile.username">
           <Button v-show="!isEditing" @click="isEditing = true">
             <Icon name="mdi:pencil" /> Editar perfil
           </Button>
@@ -134,6 +176,11 @@ watch(user, () => {
             <Icon name="mdi:content-save" /> Guardar
           </Button>
         </div>
+      </section>
+    </div>
+    <div v-else-if="profile === null">
+      <section class="flex justify-center w-full">
+        <p class="text-3xl text-center">Usuario no encontrado</p>
       </section>
     </div>
   </NuxtLayout>
