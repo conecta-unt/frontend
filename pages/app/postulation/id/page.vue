3612e5b11479b6e33c4921a92a2ec3ea25e8c950
angelchavez19 <infoangelchavez@gmail.com>
2025-07-14 00:02:56 -0500

    feat(postulation): show postulation button only when user not postuled

--- pages/app/postulation/id/page.vue
+++ pages/app/postulation/id/page.vue
@@ -1,16 +1,65 @@
 <script setup lang="ts">
 import type { OfferDetalledI } from "~/types/offer";
 
+interface OfferI extends OfferDetalledI {
+  postulationStatus: "solo" | "team" | null;
+}
+
+interface TeamI {
+  id: number;
+  name: string;
+  ownerUsername: string;
+}
+
 const { $axios } = useNuxtApp();
 const route = useRoute();
 
-const offer = ref<OfferDetalledI | null>();
-const teams = ref();
+const offer = ref<OfferI | null>();
+const teams = ref<TeamI[]>();
+const teamOptions = computed(() => {
+  if (!teams.value) return [];
+  return teams.value.map((team) => ({
+    value: team.id,
+    text: `${team.name} - @${team.ownerUsername}`,
+  }));
+});
+const teamSelected = ref<number>();
+
+const isValidData = computed(() => {
+  if (offer.value?.team && teamSelected.value) return true;
+  if (offer.value?.team === false) return true;
+  return false;
+});
+
+const sendPostulation = async () => {
+  if (!offer.value) return;
+
+  const data = {
+    offerId: offer.value.id,
+    teamId: teamSelected.value,
+  };
+
+  try {
+    await $axios.post("/postulation/create", data);
+    const result = await $axios.get<OfferI>(`/offer/${route.params.id}`);
+    offer.value = result.data;
+    alert("Se ha registrado la postulación");
+  } catch {
+    alert("Ha ocurrido un error");
+  }
+};
 
 onMounted(async () => {
   try {
-    const result = await $axios.get(`/offer/${route.params.id}`);
+    const result = await $axios.get<OfferI>(`/offer/${route.params.id}`);
     offer.value = result.data;
+
+    if (result.data.postulationStatus === null && result.data.team) {
+      const rawTeams = await $axios.get<TeamI[]>(
+        `/postulation/teams?offerId=${result.data.id}`
+      );
+      teams.value = rawTeams.data;
+    }
   } catch {
     offer.value = null;
   }
@@ -72,16 +121,36 @@ onMounted(async () => {
 
       <Separator bg-before="var(--c-primary)" />
 
-      <section v-if="offer" class="flex flex-col w-full">
-        <div v-if="offer.team" class="flex flex-col w-full">
+      <section v-if="offer" class="flex flex-col w-full gap-6">
+        <div
+          v-if="offer.postulationStatus === null && offer.team"
+          class="flex flex-col w-full"
+        >
           <div class="flex gap-4 w-full">
-            <p>Selecciona tu equipo:</p>
+            <InputSelector
+              v-model="teamSelected"
+              label="Selecciona tu equipo"
+              placeholder="Selecciona tu equipo"
+              :options="teamOptions"
+              help="Si no aparece tu equipo, es probable que los miembros no hayan aceptado la invitación."
+            />
           </div>
-          <span class="text-sm">
-            Si no aparece tu equipo, es probable que los miembros no hayan
-            aceptado la invitación.
-          </span>
         </div>
+
+        <Button
+          v-if="offer.postulationStatus === null"
+          @click="sendPostulation"
+          :disabled="!isValidData"
+        >
+          <Icon name="mdi:send" />
+          Enviar postulación
+        </Button>
+        <p v-else-if="offer.postulationStatus === 'solo'">
+          Ya has postulado a esta oferta.
+        </p>
+        <p v-else-if="offer.postulationStatus === 'team'">
+          Tu equipo ya a postulado a esta oferta.
+        </p>
       </section>
     </div>
   </NuxtLayout>
